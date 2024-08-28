5f5306707a4b62f12f5eba9304c45c6bd96d723b
Tanya <tannutanyaverma@gmail.com>
2024-08-28 12:04:17 +0530

    Updated index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const playlistRoutes = require("./routes/playlist");
 require("dotenv").config();
 const cors = require("cors");
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 app.use(cors());
 app.options("*", cors());
